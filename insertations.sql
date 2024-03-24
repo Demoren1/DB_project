a3174832f9c2bd073dd93d06794cb391972dd6e1
Alexander Udeneev <udeneev.av@phystech.edu>
2024-03-24 16:05:01 +0300

    add verisions

--- insertations.sql
+++ insertations.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS project.Ord;
-DROP TABLE IF EXISTS project.Tariff;
-DROP TABLE IF EXISTS project.Car;
-DROP TABLE IF EXISTS project.Driver;
-DROP TABLE IF EXISTS project.Loyalty_card;
-DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS project.Ord CASCADE;
+DROP TABLE IF EXISTS project.Tariff CASCADE;
+DROP TABLE IF EXISTS project.Car CASCADE;
+DROP TABLE IF EXISTS project.Driver CASCADE;
+DROP TABLE IF EXISTS project.Loyalty_card CASCADE;
+DROP TABLE IF EXISTS project.Client CASCADE;
 
 CREATE TABLE project.Tariff (
   tf_name VARCHAR(64) PRIMARY KEY,
@@ -19,11 +19,18 @@ CREATE TABLE project.Car (
 );
 
 CREATE TABLE project.Driver (
-  ID_driver INT PRIMARY KEY,
-  car_number VARCHAR(16) REFERENCES project.Car(num),
-  full_name VARCHAR(255) NOT NULL,
-  phone_number INT NOT NULL,
-  email VARCHAR(50) NOT NULL
+    ID_driver INT,
+    car_number VARCHAR(16),
+    full_name VARCHAR(255),
+    phone_number INT,
+    basic_fee INT,
+    email VARCHAR(50),
+    start_date DATE NOT NULL,
+    end_date DATE DEFAULT '9999-12-31',
+    version_number INT DEFAULT 1,
+    is_current BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (ID_driver, start_date),
+    FOREIGN KEY (car_number) REFERENCES project.Car(num)
 );
 
 CREATE TABLE project.Loyalty_card (
@@ -34,7 +41,7 @@ CREATE TABLE project.Loyalty_card (
 
 CREATE TABLE project.Client (
   ID_user INT PRIMARY KEY,
-  card_number INT REFERENCES project.Loyalty_card(card_number),
+  FOREIGN KEY card_number INT REFERENCES project.Loyalty_card(card_number),
   full_name VARCHAR(255) NOT NULL,
   phone_number INT NOT NULL,
   email VARCHAR(64) NOT NULL
@@ -42,13 +49,15 @@ CREATE TABLE project.Client (
 
 CREATE TABLE project.Ord (
   ID_ord INT PRIMARY KEY,
-  ID_user INTEGER REFERENCES project.Client(ID_user),
-  ID_driver INTEGER REFERENCES project.Driver(ID_driver),
-  car_number VARCHAR(16) REFERENCES project.Car(num),
-  tariff_name VARCHAR(64) REFERENCES project.Tariff(tf_name),
+  FOREIGN KEY ID_user INTEGER REFERENCES project.Client(ID_user),
+  FOREIGN KEY ID_driver INTEGER REFERENCES project.Driver(ID_driver),
+  FOREIGN KEY car_number VARCHAR(16) REFERENCES project.Car(num),
+  FOREIGN KEY tariff_name VARCHAR(64) REFERENCES project.Tariff(tf_name),
   start_address VARCHAR(255) NOT NULL,
   end_address VARCHAR(255) NOT NULL,
   start_time TIMESTAMP NOT NULL,
   end_time TIMESTAMP NOT NULL,
   status VARCHAR(64) NOT NULL
 );
+
+
